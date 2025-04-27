13d3058a8dcd3e58b7e25cc5c4d9bfdaf99b63af
sheepfish5 <sheepfish5@gmail.com>
2025-04-27 23:05:16 +0800

    Add commons.py, json_process.py and run_test.py

---
+++ run_test.py
@@ -0,0 +1,9 @@
+from run_image_edit import generate_image
+from json_process import *
+
+def action(image_path: str, single_image: SingleImage):
+    
+
+if __name__ == "__main__":
+    meta_data = MetaData.load()
+    # 遍历所有图片
