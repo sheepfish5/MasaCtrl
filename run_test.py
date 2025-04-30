1cab8a73ab45a1dc717c31a1d8ba107b3f87bcea
sheepfish5 <sheepfish5@gmail.com>
2025-04-30 13:44:15 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -4,7 +4,20 @@ from json_process import *
 output_dir = Path("output_dir")
 output_dir.mkdir(parents=True, exist_ok=True)
 
+# 直接运行测试
+image_data_list = {
+    "spring": [i for i in range(8, 53+1)],
+    "summer": [i for i in range(7, 53+1)],
+    "autumn": [i for i in range(5, 52+1)],
+    "winter": [i for i in range(13, 54+1)],
+}
+
 def action(image_path: str, single_image: SingleImage):
+
+    global image_data_list
+
+    if single_image.id not in image_data_list[single_image.season.value]:
+        return
     
     generate_image(image_path, single_image, output_dir)
 
