b85fb8973d9f1282e26e585582cf341247edad70
sheepfish5 <sheepfish5@gmail.com>
2025-05-06 16:35:06 +0800

    保存计算结果

---
+++ compute_is_fid.py
@@ -0,0 +1,61 @@
+from json_process import *
+import torch
+from torchvision import transforms
+from PIL import Image
+
+transform = transforms.PILToTensor()
+
+def search_target_image_path_by(source_image: SingleImage, target_season: Season) -> str:
+    result = f"output_dir/{source_image.season.value}-{source_image.id:02d}-to-{target_season.value}.jpg"
+    return result
+
+
+def path_to_tensor(image_path: str) -> torch.Tensor:
+    """
+        返回值：
+            torch.Tensor(1, 3, 299, 299)
+    """
+    return transform(Image.open(image_path).resize((299,299))).unsqueeze(0)
+
+from torchmetrics.image.inception import InceptionScore
+from torchmetrics.image.fid import FrechetInceptionDistance
+
+total_fid = FrechetInceptionDistance(feature=64)
+total_is = InceptionScore()
+
+
+SEASONS = [Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER]
+def action(image_path: str, single_image: SingleImage):
+
+    source_image_tensor = path_to_tensor(image_path)
+    total_fid.update(source_image_tensor, real=True)
+
+    for target_season in SEASONS:
+        if target_season == single_image.season: continue
+
+        target_image_path = search_target_image_path_by(single_image, target_season)
+        assert target_image_path != ""
+        
+        image_tensor = path_to_tensor(target_image_path)
+        total_fid.update(image_tensor, real=False)
+        total_is.update(image_tensor)
+
+
+
+if __name__ == '__main__':
+    meta_data = MetaData.load()
+    meta_data.traverse_images(action)
+
+    avg_fid = total_fid.compute()
+    is_mean, is_std = total_is.compute()
+
+    with open("./fid_is.json", "w") as f:
+        json.dump({
+            "fid": avg_fid.item(),
+            "is_mean": is_mean.item(),
+            "is_std": is_std.item()
+        }, f, indent=4)
+    
+    print(f"avg_fid: {avg_fid.item()}")
+    print(f"is_mean: {is_mean.item()}")
+    print(f"is_std: {is_std.item()}")
